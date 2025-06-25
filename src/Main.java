6dfa23468e260b20261d75f4a7ba2572e2c8d4ad
Anoushka Singh <anoushkadivyas@gmail.com>
2025-06-25 12:36:10 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Main source file, exists only to launch application.
+
+// Import package containing application.
+import application.*;
+
+// Set up the class.
+public class Main {
+    // Entry point.
+    public static void main(String[] args) {
+        // Just start the application.
+        new Login();
+    }
+}
